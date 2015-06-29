ad4c5de429cbdfe1b1c210e68bd4d5d46338fb43
Wladimir Sidorenko <wlsidorenko@gmail.com>
2015-06-30 01:17:12 +0200

    extract_cc updated

--- scripts/extract_cc.py
+++ scripts/extract_cc.py
@@ -18,15 +18,20 @@ from itertools import combinations
 
 import argparse
 import codecs
+import re
+import string
 import sys
 
 ##################################################################
 # Variables and Constants
+VERBOSE = False
+PUNCT_RE = re.compile(r"^(?:" + '|'.join([re.escape(c) for c in string.punctuation]) + \
+                          ")+$")
 ESC_CHAR = ""
 ENCODING = "utf-8"
 CC_RELATIONS = set(["CD", "CJ"])
-ADVERS_CC = set([normalize(w) for w in ("aber")])
-COORD_CC = set([normalize(w) for w in ("und", "oder")])
+ADVERS_CC = set([normalize(w) for w in ["aber"]])
+COORD_CC = set([normalize(w) for w in ["und", "oder", ","]])
 
 ##################################################################
 # Methods
@@ -78,6 +83,62 @@ def _process_cc_helper(a_tree, a_iroot, a_cc_main, a_cc_seen = False):
     else:
         return []
 
+def _output_cc_helper(a_cc_list):
+    """
+    Extract sets of coordinatively and adversatively conjoined terms
+
+    @param a_cc_list - list of conjoined phrases
+
+    @return 2-tuple with sets of coordinatively and adversatively conjoined terms
+    """
+    coord = set(); advers = set()
+    ret = (coord, advers)
+    trg_coord = trg_advers = chld_coord = chld_advers = None
+
+    chld_ret = None
+    if type(a_cc_list) == list:
+        if not a_cc_list:
+            return ret
+        a_cc_list[0] = normalize(a_cc_list[0])
+        if a_cc_list[0] in ADVERS_CC:
+            trg_coord, trg_advers = advers, coord
+        else:
+            if a_cc_list[0] not in COORD_CC:
+                if VERBOSE:
+                    print("WARNING: Unknown coordinative conjunction: '{:s}'".format(repr(a_cc_list[0])), \
+                              file = sys.stderr)
+                coord.add(a_cc_list[0])
+            trg_coord, trg_advers = coord, advers
+        for chld in a_cc_list[1:]:
+            chld_coord, chld_advers = _output_cc_helper(chld)
+            trg_coord |= chld_coord; trg_advers |= chld_advers
+    else:
+        coord.add(a_cc_list)
+    return ret
+
+def _output_cc(a_cc_list):
+    """
+    Output coordinatively conjoined phrases
+
+    @param a_cc_list - list of coordinatively conjoined phrases
+
+    @return \c void
+    """
+    coord = advers = None
+    for cc in a_cc_list:
+        coord, advers = _output_cc_helper(cc)
+        coord = set(w for w in coord if not PUNCT_RE.match(w))
+        advers = set(w for w in advers if not PUNCT_RE.match(w))
+        # output all possible pairs of coordinatively conjoined phrases
+        for w1, w2 in combinations(coord, 2):
+            print("{:s}\t{:s}\t1".format(w1, w2).encode(ENCODING))
+        for w1, w2 in combinations(advers, 2):
+            print("{:s}\t{:s}\t1".format(w1, w2).encode(ENCODING))
+        # output all possible pairs of adversatively conjoined phrases
+        for w1 in coord:
+            for w2 in advers:
+                print("{:s}\t{:s}\t-1".format(w1, w2).encode(ENCODING))
+
 def process_cc(a_tree):
     """
     Output all coordinarively conjoined phrases found in DG tree
@@ -92,8 +153,10 @@ def process_cc(a_tree):
     # find trees roots
     for iroot in _find_roots(a_tree):
         _process_cc_helper(a_tree, iroot, cc_list, False)
-    # extract coordinarively conjoined chains
-    print(repr(cc_list), file = sys.stderr)
+        # extract coordinarively conjoined chains
+        # print(repr(cc_list), file = sys.stderr)
+        _output_cc(cc_list)
+        del cc_list[:]
     # clear sentence
     a_tree.clear()
 
